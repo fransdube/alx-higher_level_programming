1eec57894c7a3bc2944bb2773a777b2fc8fa8588
FM Dube <31796906+fransdube@users.noreply.github.com>
2023-05-12 22:48:07 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+import sys
+if __name__ == '__main__':
+    sys.argv.pop(0)
+    total = 0
+    for number in sys.argv:
+        total += int(number)
+    print("{}".format(total))
